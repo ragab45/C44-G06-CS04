e920536ff5997aaee13594dff9ed1bda63e63bfd
ragab45 <115900223+ragab45@users.noreply.github.com>
2025-07-03 05:40:41 +0300

    Video_2

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -48,6 +48,51 @@
                     break;
             }
             #endregion
+
+            #region  Video_2
+         //Switch Case Before Switch Expression
+            // Example 01 - Pattern matching without alias name
+
+            string option = Console.ReadLine();
+            string message;
+
+            switch (option)
+            {
+                case "1":
+                    message = "Option 01";
+                    break;
+                case "2":
+                    message = "Option 02";
+                    break;
+                case "3":
+                    message = "Option 03";
+                    break;
+                default:
+                    message = "Invalid Option";
+                    break;
+            }
+
+            Console.WriteLine(message);
+
+
+           // Example 04 [Nullable Type - Relational Patterns]
+
+            int? X = 10;
+            X = null;
+
+            string message = X switch
+            {
+                null => "Null Value",
+                int number when number > 0 => "Positive Number",
+                int number when number < 0 => "Negative Number",
+                0 => "Zero",
+                // The default case isn't required here if all possibilities of int? are handled
+            };
+
+            Console.WriteLine(message);
+
+            
+            #endregion
         }
     }
 }
