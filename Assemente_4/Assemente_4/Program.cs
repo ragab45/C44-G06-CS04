7484045f8cb77b9570bc691def31129f72d49483
ragab45 <115900223+ragab45@users.noreply.github.com>
2025-07-03 05:59:57 +0300

    Q-2

---
+++ Assemente_4/Assemente_4/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Assemente_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region  Q-1
+            Console.Write("Enter a number: ");
+            if (int.TryParse(Console.ReadLine(), out int number))
+            {
+                Console.WriteLine((number % 3 == 0 && number % 4 == 0) ? "Yes" : "No");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid integer.");
+            }
+
+            #endregion
+            #region Q-2
+            Console.Write("Enter an integer: ");
+            if (int.TryParse(Console.ReadLine(), out int num2))
+            {
+                Console.WriteLine(num2 < 0 ? "negative" : "positive");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input."); 
+            }
+            #endregion
+        }
+    }
+}
