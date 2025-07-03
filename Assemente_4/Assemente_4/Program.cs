f5f04c7d9a062ddcb059fb84eb82f083cabf633d
ragab45 <115900223+ragab45@users.noreply.github.com>
2025-07-03 06:00:38 +0300

    Q-3

--- Assemente_4/Assemente_4/Program.cs
+++ Assemente_4/Assemente_4/Program.cs
@@ -27,6 +27,22 @@
                 Console.WriteLine("Invalid input."); 
             }
             #endregion
+            #region  Q-3
+            Console.Write("Enter 3 numbers separated by space: ");
+            string[] inputs = Console.ReadLine()?.Split();
+            if (inputs?.Length == 3 && int.TryParse(inputs[0], out int a) && int.TryParse(inputs[1], out int b) && int.TryParse(inputs[2], out int c))
+            {
+                int max = Math.Max(a, Math.Max(b, c));
+                int min = Math.Min(a, Math.Min(b, c));
+                Console.WriteLine($"Max element = {max}");
+                Console.WriteLine($"Min element = {min}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter exactly 3 integers.");
+            }
+
+            #endregion
         }
     }
 }
